b709bafab9b4da84fea8408619f90ae6ac433a94
전수열 <devxoul@gmail.com>
2012-12-21 22:44:19 +0900

    Initial Commit

---
+++ Dubbbing/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Dubbbing
+//
+//  Created by 전수열 on 12. 12. 21..
+//  Copyright (c) 2012년 Joyfl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Dubbbing/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Dubbbing
+//
+//  Created by 전수열 on 12. 12. 21..
+//  Copyright (c) 2012년 Joyfl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
